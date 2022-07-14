9b7fde2af86a936392ce3c0c88da5d4abc5efe16
matthew-hocking <matthocking2439@gmail.com>
2022-07-15 11:26:48 +1200

    app failsafe

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react'
+
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isSSR, setIsSSR] = useState(true)
+
+  useEffect(() => {
+    setIsSSR(false)
+  }, [])
+
+  if(isSSR) return null
+  
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
