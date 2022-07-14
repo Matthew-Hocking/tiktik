8c5459b6db5a2094e604ee88a6a1d32fc5b7578a
matthew-hocking <matthocking2439@gmail.com>
2022-07-15 11:59:27 +1200

    navbar and sidebar init, and downloading utils

---
+++ components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { GoogleLogin, GoogleLogout } from 'react-google-login'
+import { AiOutlineLogout } from 'react-icons/ai'
+import { BiSearch } from 'react-icons/bi'
+import { IoMdAdd } from 'react-icons/io'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link>
+        <div>
+          <Image
+            className='cursor-pointer'
+            src={}
+          >
+
+          </Image>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
 
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -14,7 +16,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   
   return (
     <div>
-      <Component {...pageProps} />
+      <Navbar />
+      <div className='flex gap-6 md:gap-20'>
+        <div className='h-[92vh] overflow-hidden xl:overflow-auto'>
+          <Sidebar />
+        </div>
+        <div className='mt-4 flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+          <Component {...pageProps} />
+        </div>
+      </div>
     </div>
   )
 }
